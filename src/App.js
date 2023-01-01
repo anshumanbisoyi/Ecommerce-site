d52a9ce473166a7de5b227e87b2a9b3c693ca5b2
Anshuman Bisoyi <ansuman746@gmail.com>
2023-01-02 03:46:42 +0530

    added offer line

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Annoucement from "./components/Annoucement";
 import Navbar from "./components/Navbar";
+
 
 const App = () => {
   return <div>
+  <Annoucement/>
     <Navbar/>
   </div>;
 };
--- src/components/Annoucement.jsx
+++ src/components/Annoucement.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import styled from 'styled-components';
 
+const Container = styled.div`
+height:30px;
+background-color: grey;
+color: white;
+display: flex;
+align-items: center;
+justify-content: center;
+font-size:14px ;
+
+`;
 
 const Annoucement = () => {
   return (
-    <div>Annoucement</div>
-  )
+    <Container>10% off 1st purchase + Free shipping over Rs 999.</Container>
+  );
 }
 
 export default Annoucement
