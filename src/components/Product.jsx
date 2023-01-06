8ee6bf6d1fd2088dd4ce47eb8a5dd2e3f3bc7b55
Anshuman Bisoyi <ansuman746@gmail.com>
2023-01-07 00:23:32 +0530

    added products

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -17,8 +17,8 @@ const Info = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  transition: all 0.5s ease;
   cursor: pointer;
+  transition: all 0.5s ease;
 `;
 
 const Container = styled.div`
